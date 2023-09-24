c7b2f252110114a6a7c5fcaad287eead59ab8cdc
StinkySteak <slimesteve17@gmail.com>
2023-09-24 22:31:46 +0700

    chore: remove playerCollider

--- Assets/Scripts/PlayerColliderConfig.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = nameof(PlayerColliderConfig), menuName = "Data/PlayerCollider")]
-public class PlayerColliderConfig : ScriptableObject
-{
-    [SerializeField] private float _sphereRadius;
-
-    public float SphereRadius => _sphereRadius;
-}

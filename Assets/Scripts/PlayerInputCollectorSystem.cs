39a9237184b3fc919453aaeeb3a835947406a447
StinkySteak <slimesteve17@gmail.com>
2023-09-24 23:18:24 +0700

    feat: added mobile input

--- Assets/Scripts/PlayerInputCollectorSystem.cs
+++ Assets/Scripts/PlayerInputCollectorSystem.cs
@@ -10,22 +10,65 @@ using Scellecs.Morpeh;
 public sealed class PlayerInputCollectorSystem : UpdateSystem
 {
     private Filter _filter;
+    private Filter _mobileInputFilter;
     private const string HORIZONTAL = "Horizontal";
 
     public override void OnAwake()
     {
         _filter = World.Filter.With<ActivePlayerInput>().Build();
+
+        CreateMobileInput();
+    }
+
+    private void CreateMobileInput()
+    {
+        _mobileInputFilter = World.Filter.With<ActiveMobileInput>().Build();
+
+        Entity e = World.CreateEntity();
+        e.AddComponent<ActiveMobileInput>();
     }
 
     public override void OnUpdate(float deltaTime)
     {
-        float horizontal = Input.GetAxisRaw(HORIZONTAL);
-        
+        CollectMobileInput();
+
+        float horizontal = GetInput();
+
         foreach (Entity e in _filter)
         {
             ref ActivePlayerInput playerInput = ref e.GetComponent<ActivePlayerInput>();
 
             playerInput.Horizontal = horizontal;
         }
+    }
+
+    private void CollectMobileInput()
+    {
+        if (!Input.GetMouseButton(0)) return;
+
+        Entity e = _mobileInputFilter.First();
+        ref ActiveMobileInput mobileInput = ref e.GetComponent<ActiveMobileInput>();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mobileInput.TouchPositionX = Input.mousePosition.x;
+        }
+
+        float currentMousePositionX = Input.mousePosition.x;
+        float previousMousePositionX = mobileInput.TouchPositionX;
+
+        float delta = (currentMousePositionX - previousMousePositionX) / 2;
+
+        mobileInput.TouchPositionX = currentMousePositionX;
+        mobileInput.DeltaX = delta;
+    }
+
+    private float GetInput()
+    {
+        Entity e = _mobileInputFilter.First();
+
+        ActiveMobileInput mobileInput = e.GetComponent<ActiveMobileInput>();
+
+        return mobileInput.DeltaX;
     }
 }
